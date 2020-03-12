dc5b9fcb199cc154a7ec8d230a3088afc4f35f0a
Alexis Sonolet <Alexis.Sonolet@grenoble-inp.org>
2020-03-12 08:57:23 +0100

    TP1 over

--- TP1_sonoleta/src/Dvector.cpp
+++ TP1_sonoleta/src/Dvector.cpp
@@ -1,16 +1,79 @@
 #include "Dvector.h"
+#include <fstream>
 
 Dvector::Dvector()
 {
     v = new double[0]; // v vecteur de taille définie
+    v_size = 0;
 };
 
 Dvector::Dvector(int size, int value)
 {
     v = new double[size]; // v vecteur de taille définie
+    v_size = size;
     for (int i = 0; i < size; i++)
         v[i] = value;
 };
+
+Dvector::Dvector(Dvector const& vect)
+{
+    v = new double[vect.v_size];
+    v_size = vect.v_size;
+    for (int i = 0; i < vect.v_size; i++)
+        v[i] = *(vect.v + i);
+};
+
+Dvector::Dvector(std::string fichier)
+{
+
+    // Lit le fichier dans string
+    std::ifstream flux(fichier);
+    int vect_size = 0;
+    if(flux)
+    {
+        std::string ligne; //Une variable pour stocker les lignes lues
+        while(getline(flux, ligne)) //Tant qu'on n'est pas à la fin, on lit
+        {
+            int n;
+            try {
+                n = std::stoi(ligne);
+                vect_size++;
+            }
+            catch(...) {
+                // TODO : éviter les "..." => trouver la bonne erreur
+            }
+        }
+    }
+    else
+    {
+        v = new double[0];
+        v_size = 0;
+        return;
+    }
+    v = new double[vect_size];
+    v_size = vect_size;
+
+    flux.clear();
+    flux.seekg(0, flux.beg);
+    
+    int i = 0;
+    if(flux)
+    {
+        std::string ligne; //Une variable pour stocker les lignes lues
+        while(getline(flux, ligne)) //Tant qu'on n'est pas à la fin, on lit
+        {
+            int n;
+            try {
+                n = std::stoi(ligne);
+                i++;
+                v[i] = n;
+            }
+            catch(...) {
+                // TODO : éviter les "..." => trouver la bonne erreur
+            }
+        }
+    }
+}
 
 Dvector::~Dvector()
 {
@@ -19,8 +82,21 @@ Dvector::~Dvector()
 
 void Dvector::display(std::ostream& str)
 {
-    for (long unsigned int i = 0; i< sizeof(Dvector)/ sizeof(double); i++) {
+    for (long unsigned int i = 0; i< v_size; i++) {
         str << v[i];
         str << "/n";
+    }
+};
+
+int Dvector::size()
+{
+    return v_size;
+};
+
+void Dvector::fillRandomly()
+{
+    for(long unsigned int i = 0; i<v_size; i++) {
+        // fill with random
+        v[i] = rand();
     }
 };
--- TP1_sonoleta/src/Dvector.h
+++ TP1_sonoleta/src/Dvector.h
@@ -7,9 +7,14 @@ class Dvector
     public :
         Dvector();
         Dvector(int size, int value=0);
+        Dvector(Dvector const& vect);
+        Dvector(std::string fichier);
         ~Dvector();
         void display(std::ostream& str);
+        int size();
+        void fillRandomly();
     
     private:
         double* v;
+        int v_size;
 };
