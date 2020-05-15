bd77f97d2d4752321d4c8da511936b0a01915eee
Louis Soum-Fontez <louissoumfontez@gmail.com>
2020-05-15 10:22:45 +0200

    ifft now works (still imprecision due to pi)

--- TP4_sonoleta/headers/ComplexVector.h
+++ TP4_sonoleta/headers/ComplexVector.h
@@ -236,7 +236,7 @@ ComplexVector<T> ComplexVector<T>::get_even() const
 template<typename T>
 ComplexVector<T> ComplexVector<T>::get_odd() const
 {
-	ComplexVector odd((int)(v_size/2));
+	ComplexVector<T> odd((int)(v_size/2));
 	for(int index = 1; index < v_size; index+=2)
 	{
 		odd.set((int)(index/2), v[index]);
@@ -247,12 +247,12 @@ ComplexVector<T> ComplexVector<T>::get_odd() const
 template<typename T>
 ComplexVector<T> ComplexVector<T>::conjug() const
 {
-	ComplexVector conjugate(v_size);
+	ComplexVector<T> conjugate(v_size);
 	for(int index = 0; index < v_size; index++)
 	{
 		conjugate.set(index, conj(v[index]));
 	}
-
+	return conjugate;
 }
 template<typename T>
 ComplexVector<T> ComplexVector<T>::operator=(const ComplexVector &vect)
